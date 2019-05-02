82902328f1c2b6627d8428d808b08ea9af42b351
MatthewTSwain <shannonswain@Shannons-MacBook-Pro.local>
2019-05-02 09:58:42 -0700

    First Commit from Local
    First commit form local to my final project.

---
+++ _js/test.js
@@ -0,0 +1,16 @@
+console.log("Hello world!");
+var x = 42;
+var y = 54;
+var z = (x + y);
+console.log(x + y);
+// alert(z);
+console.log();
+const square = function(x) {
+    return x * x;
+  };
+  
+  console.log(square(12));
+  // → 144
+var myHeading = document.querySelector('h1');
+myHeading.textContent = (math.floor(math.random()(*20)+1);
+
